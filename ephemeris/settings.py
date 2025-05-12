22d9102d5679abd7f96ef3f88eb23a64d31d4e8e
Jacob McSwain <jacob@mcswain.dev>
2025-05-12 08:37:45 -0500

    Add DOC_MONOCHROME as an option (#29)
    The grayish default values used for text and other objects can cause reading issues after being rendered out to a single bit per pixel. This option tells ephemeris to only use 2 colors and adjusts the calls to `pdftocairo` to disable anti-aliasing to avoid blurry text.

--- ephemeris/settings.py
+++ ephemeris/settings.py
@@ -89,6 +89,7 @@ ALLDAY_IN_GRID     = ALLDAY_MODE == "in-grid"
 DRAW_ALL_DAY       = ALLDAY_MODE in ("band", "in-grid")
 
 # Color defaults
+MONOCHROME      = os.getenv("DOC_MONOCHROME", "false").lower() in ("1", "true", "yes")
 EVENT_FILL      = os.getenv("DOC_EVENT_FILL_COLOR", "gray14")
 EVENT_STROKE    = os.getenv("DOC_EVENT_BORDER_COLOR", "gray(20%)")
 GRIDLINE_COLOR  = os.getenv("DOC_GRID_LINE_COLOR", "gray(20%)")
